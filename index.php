21fb767a8ebfac1382bb7ba86a0585576fd32ba5
oravraino <168183364+oravraino@users.noreply.github.com>
2024-04-26 14:21:37 +0300

    Update index.php
    pilt

--- index.php
+++ index.php
@@ -4,5 +4,7 @@
   </head>
   <body>
     <h1>Meie projeti pealeht</h1>
+    <p>See leht arvestab ka muudatustega</p>
+    <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/32c8/live/8ab8d810-bf51-11ee-a5c6-319f8a960f8f.jpg.webp">
   </body>  
 </html>
